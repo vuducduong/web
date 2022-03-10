2682409a63a77e7b57f87e6d13663823b2dfea9e
Vũ Đức  Dương <vd-duong@jit.vn>
2022-03-10 17:04:23 +0700

    up date code

---
+++ database/migrations/2022_03_10_085406_create_chapters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('story_id');
+            $table->string('name');
+            $table->string('number');
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
